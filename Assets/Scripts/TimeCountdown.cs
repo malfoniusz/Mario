05ebabc5e5fa8ae358467494e6b883b5c3a2f3a4
Piotr Osipa <piotr.osipa@gmail.com>
2017-08-23 16:21:58 +0200

    time countdown + hurry music

---
+++ Assets/Scripts/TimeCountdown.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCountdown : MonoBehaviour
+{
+    public AudioSource environmentMusic;
+    public AudioClip hurryMusic;
+    public float countdownSpeed;
+    public int time;
+
+    private Text text;
+    private float nextTimeInc;
+    private float deltaTime = 0;
+    private bool lowTime = false;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        nextTimeInc = 1 / countdownSpeed;
+    }
+
+    void Start()
+    {
+        SetText(time);
+    }
+
+    void Update()
+    {
+        TimeCounting();
+        HurryMusic();
+    }
+
+    void TimeCounting()
+    {
+        deltaTime += Time.deltaTime;
+        if (deltaTime > nextTimeInc)
+        {
+            time -= 1;
+            deltaTime = 0;
+            SetText(time);
+        }
+    }
+
+    void HurryMusic()
+    {
+        if (time <= 100 && lowTime == false)
+        {
+            lowTime = true;
+            environmentMusic.clip = hurryMusic;
+            environmentMusic.Play();
+        }
+    }
+
+    void SetText(int time)
+    {
+        text.text = time.ToString();
+    }
+
+}
