d402b6440c9eb7036e9b971beedee4cda02f283f
Piotr Osipa <piotr.osipa@gmail.com>
2017-08-28 16:08:08 +0200

    animacja punktow zdobytych z monety z bloku zapytania

--- Assets/Scripts/CoinFromBlock.cs
+++ Assets/Scripts/CoinFromBlock.cs
@@ -2,11 +2,17 @@
 
 public class CoinFromBlock : Coin
 {
+    public GameObject pointsFloating;
+
     private Animator anim;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
         UICoins.AddCoins(1);
         UIPoints.AddPoints(points);
     }
@@ -15,6 +21,10 @@ public class CoinFromBlock : Coin
     {
         if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
         {
+            GameObject pointsObject = Instantiate(pointsFloating);
+            pointsObject.transform.GetChild(0).position = transform.GetChild(0).position;
+            pointsObject.GetComponent<PointsFloating>().SetPoints(points);
+
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/PointsFloating.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsFloating : MonoBehaviour
+{
+    private Animator anim;
+    private Text text;
+    private int points = 0;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        text = transform.GetChild(0).GetComponent<Text>();
+        UpdateText();
+    }
+
+    void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void UpdateText()
+    {
+        text.text = points.ToString();
+    }
+
+    public void SetPoints(int points)
+    {
+        this.points = points;
+        UpdateText();
+    }
+}
