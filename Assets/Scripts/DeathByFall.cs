e1a01f8f2457e2a288c25b7c66301af31e287b13
Piotr Osipa <piotr.osipa@gmail.com>
2017-09-11 16:25:57 +0200

    smierc od skoku w przepasc

---
+++ Assets/Scripts/DeathByFall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DeathByFall : MonoBehaviour
+{
+    public PlayerDeath playerDeath;
+
+    void Awake()
+    {
+        playerDeath = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDeath>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerDeath.Die();
+        }
+    }
+
+}
