5591e756765481fb378106c731849f37ea80fa86
Piotr Osipa <piotr.osipa@gmail.com>
2017-09-28 14:23:37 +0200

    destroy brick block when BigMario

---
+++ Assets/Scripts/Blocks/Brick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Brick : BlockAnimated
+{
+    public GameObject brickBroken;
+
+    private GameObject player;
+    private PlayerPowerup playerPowerup;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerPowerup = player.GetComponent<PlayerPowerup>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (playerHit && playerPowerup.level > 1)
+        {
+            Instantiate(brickBroken, transform.localPosition, Quaternion.identity);
+            Destroy(parent);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Blocks/BrickBroken.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickBroken : MonoBehaviour
+{
+    public GameObject[] pieces;
+    public float rotationValue = 60f;
+
+    private Rigidbody2D[] rbPieces;
+
+    private void Awake()
+    {
+        rbPieces = new Rigidbody2D[pieces.Length];
+
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            rbPieces[i] = pieces[i].GetComponent<Rigidbody2D>();
+        }
+    }
+
+    private void Start()
+    {
+        PushPieces();
+    }
+
+    private void FixedUpdate()
+    {
+        RotatePieces();
+    }
+
+    private void PushPieces()
+    {
+        const float FORCE_X = 4000f;
+        const float FORCE_Y = 13000f;
+
+        const float FORCE_X_LOWER = FORCE_X * ((float)2 / 3);
+        const float FORCE_Y_HIGHER = FORCE_Y * ((float)4 / 3);
+
+        FlyAway(rbPieces[0], new Vector2(-FORCE_X, FORCE_Y_HIGHER));
+        FlyAway(rbPieces[1], new Vector2(FORCE_X, FORCE_Y_HIGHER));
+        FlyAway(rbPieces[2], new Vector2(-FORCE_X_LOWER, FORCE_Y));
+        FlyAway(rbPieces[3], new Vector2(FORCE_X_LOWER, FORCE_Y));
+
+        StartCoroutine(WaitForDestroy(2));
+    }
+
+    private void FlyAway(Rigidbody2D rb, Vector2 dir)
+    {
+        rb.AddForce(dir);
+    }
+
+    IEnumerator WaitForDestroy(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+        Destroy(gameObject);
+    }
+
+    private void RotatePieces()
+    {
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            rbPieces[i].transform.Rotate(new Vector3(0, 0, rotationValue));
+        }
+    }
+
+}
