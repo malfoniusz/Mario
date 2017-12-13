a6955ac0fbe1b98116243e859d75115aa5d2d066
Piotr Osipa <piotr.osipa@gmail.com>
2017-12-13 16:39:50 +0100

    skakanie gwiazdy

--- Assets/Scripts/Items/StarPowerup.cs
+++ Assets/Scripts/Items/StarPowerup.cs
@@ -2,6 +2,50 @@
 
 public class StarPowerup : Mushroom
 {
+    public Transform[] groundChecks;
+    public float jumpStrength = 260f;
+    public float gravityScale = 0.5f;
+
+    private bool feltFromBlock = false;
+    private bool grounded = true;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        rb.gravityScale = gravityScale;
+    }
+
+    private void Update()
+    {
+        grounded = Contact.CheckContactGround(transform.position, groundChecks);
+        SwitchFelt();
+    }
+
+    private void SwitchFelt()
+    {
+        if (feltFromBlock == false && grounded == false)
+        {
+            feltFromBlock = true;
+        }
+    }
+
+    protected override void MovingBehaviour()
+    {
+        base.MovingBehaviour();
+
+        if (appearAnim == false) Jumping();
+    }
+
+    private void Jumping()
+    {
+        if (grounded && feltFromBlock)
+        {
+            Vector2 jumpVel = new Vector2(rb.velocity.x, jumpStrength);
+            rb.velocity = jumpVel;
+        }
+    }
+
     protected override void CollisionBehaviour()
     {
         GrantInvincibility();
@@ -12,7 +56,7 @@ public class StarPowerup : Mushroom
 
     private void GrantInvincibility()
     {
-
+        // TODO: uzupelnic
     }
 
 }
