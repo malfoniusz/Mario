f9a90b2c45903dde9cc3957450560bef6fa8dc1d
Piotr Osipa <piotr.osipa@gmail.com>
2018-04-22 12:15:43 +0200

    - zmiana dla maria maksymalnej prędkości chodzenia i biegania - rozpoczęcie prac nad logiką związaną z dotknięciem FlagPole

---
+++ Assets/Scripts/End/FlagPole.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FlagPole : MonoBehaviour
+{
+    public BoxCollider2D[] flagPoleColliders;
+    public int[] pointsForCollider;
+    public GameObject flagPoleBase;
+    public Transform spawnPointsPos;
+
+    private bool flagTouched = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (flagTouched == false && collision.gameObject.tag == TagNames.player)
+        {
+            flagTouched = true;
+            SpawnPoints();
+        }
+    }
+
+    private void SpawnPoints()
+    {
+        //SpawnPointsFloating.Points(spawnPointsPos.position, 100);
+    }
+
+}
